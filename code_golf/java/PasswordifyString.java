7f61fea3cc83b38b06bc4e9c6787fb001370e6a3
Jessie Edmisten <jaedmisten@gmail.com>
2021-02-07 17:07:07 -0500

    Updated passwordify string code gold for Java to be more efficient.

--- code_golf/java/PasswordifyString.java
+++ code_golf/java/PasswordifyString.java
@@ -1,5 +1,5 @@
 /*
- * 7/3/2019
+ * 2/7/2021
  * 
  * Passwordify the string
  * 
@@ -16,8 +16,7 @@
 public class PasswordifyString {
 	
 	public static void main(String[] args) {
-		String[] inputStrings = {"1pass 2word3 ", "Jessie Edmisten", "12asdfg34qwert56zx", "Hello world", 
-						"pa55w0rd", "14 35", "0971", " "};
+		String[] inputStrings = {"1pass 2word3 ", "Jessie Edmisten", "12asdfg34qwert56zx", "Hello world", "pa55w0rd", "14 35", "0971", " "};
 		
 		for (String inputStr : inputStrings) {
 			System.out.println(inputStr);
@@ -27,31 +26,19 @@ public class PasswordifyString {
 	
 	public static String passwordifyString(String inputStr) {
 		String digitStr = ""; 
-		String randStr = "";
+		String charStr = "";
 		
 		inputStr = inputStr.replace(" ", "_");
 		
-		// Create string of digits from input string in the order they appeared from left to right.
 		for (int i = 0; i < inputStr.length(); i++) {
 			if (Character.isDigit(inputStr.charAt(i))) {
 				digitStr += inputStr.charAt(i);
+			} else {			
+				int randValue = (int) Math.round(Math.random());
+				charStr += (randValue == 0) ? Character.toLowerCase(inputStr.charAt(i)) : Character.toUpperCase(inputStr.charAt(i)); 
 			}
 		}
 		
-		// Remove digits from input string.
-		inputStr = inputStr.replaceAll("([0-9])", "");
-		
-		// Randomly make letters in input string upper or lower case.
-		for (int i = 0; i < inputStr.length(); i++) {
-			int randValue = (int) Math.round(Math.random());
-			if (randValue == 0) {
-				randStr += Character.toLowerCase(inputStr.charAt(i));
-			} else {
-				randStr += Character.toUpperCase(inputStr.charAt(i));
-			}
-		}
-		
-		return randStr + digitStr;
+		return charStr + digitStr;
 	}
-
 }
