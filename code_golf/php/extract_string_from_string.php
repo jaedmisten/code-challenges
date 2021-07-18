2b75c644d2dc6613b9e038914d85447500fb184c
Jessie Edmisten <jaedmisten@gmail.com>
2021-07-17 21:00:45 -0400

    Updated PHP code golf extract string from string

--- code_golf/php/extract_string_from_string.php
+++ code_golf/php/extract_string_from_string.php
@@ -20,37 +20,21 @@ $inputStrings = [
     ["agdagaga", "a"],
     ["Last test-case", "  -"],
 ];
-echo '<pre>';
-print_r($inputStrings);
-echo '</pre>';
 
 for ($i = 0; $i < count($inputStrings); $i++) {
-    //echo "<br>";
-
     echo $inputStrings[$i][0] . "<br>";
     echo $inputStrings[$i][1] . "<br>";
-    //echo $str . "<br>";
-
     $innerStr = getInnerString($inputStrings[$i][0], $inputStrings[$i][1]);
-
     echo "$innerStr<br><br>";
-    //echo "<br>";
 }
 
 function getInnerString($str, $endpoints) {
-    //echo "$str: {$str} <br>";
-    //echo "\$endpoints: $endpoints<br>";
-    //echo "endpoints[0]: $endpoints[0]<br>";
-    //echo "endpoints[2]: $endpoints[2]<br>";
-
     if (strlen($str) < 3 || strlen($str) > 100) {
         return "Incorrect Input: Input string must be between 3 and 100 characters.<br>";
     }
-
     if (hasInvalidChars($str) || hasInvalidChars($endpoints)) {
         return "Incorrect Input: Input string and endpoints string can only have characters between a space character (32) and a tilde character '~' (127);<br>";
     }
-
     if ($endpoints && (strlen($endpoints) == 3) && ($endpoints[1] == " ") && ($endpoints[0] !== $endpoints[2])) {
         $endpoint1 = $endpoints[0];
         $endpoint2 = $endpoints[2];
@@ -59,16 +43,11 @@ function getInnerString($str, $endpoints) {
     }
 
     $endpoint1Position = strpos($str, $endpoint1);
-    //echo "position1: $endpoint1Position<br>";
-
     $endpoint2Position = strpos($str, $endpoint2);
-    //echo "position2: $endpoint2Position<br>";
 
     if (isEndpointDuplicated($str, $endpoint1) || isEndpointDuplicated($str, $endpoint2)) {
-        //echo '"null"' . "<br>";
         return '"null"';
     }
-
     if ($endpoint1Position < $endpoint2Position) {
         $innerStr = substr($str, $endpoint1Position + 1, $endpoint2Position - $endpoint1Position - 1);
     } else {
